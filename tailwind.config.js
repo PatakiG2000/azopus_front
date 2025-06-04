7c32c3b744ecf63ee31c9a678746f5b5f696eaa5
PatakiG <111224225+PatakiG2000@users.noreply.github.com>
2025-06-04 17:49:29 +0200

    feat tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#E5E7EB",
+        primary: "#111111",
+        secondary: "#DFCF6F",
+        accent: "#FDF9ED",
+        base: "#1F2937",
+      },
+    },
+  },
+  plugins: [],
+};
